d9250b47a0abc1552e16b7127f232ca631e19674
Frank Froggins <pearlover912@gmail.com>
2025-09-02 07:50:35 -0400

    fix

--- Formula/tb64-cpp.rb
+++ Formula/tb64-cpp.rb
@@ -1,16 +1,24 @@
 class Tb64Cpp < Formula
   desc "A niche tool to encode text in base64 3 times. (C++ version)"
   homepage "https://github.com/franktankbank/tb64-cpp"
-  url "https://github.com/franktankbank/tb64-cpp/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "1c356bc1e5cfc7106f18713d98250af17e7b04abb36f527afb0471f32c8066ca"
+  url "https://github.com/franktankbank/tb64-cpp/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "7a555f565e1c880b40178d4c210e3a46ab24e8c4089fce6b6baf482ee808072b"
   license "MIT"
 
   depends_on "cmake" => :build
-  depends_on "vcpkg" => :build
+
+  resource "vcpkg" do
+    url "https://github.com/microsoft/vcpkg/archive/refs/tags/2025.08.27.tar.gz"
+    sha256 "7ca5a754e4fbaa0f1d36c5f19ceef4ed47c65312fed298bb3dcf73c276dbe9b"
+  end
 
   def install
-    vcpkg_root = ENV["VCPKG_ROOT"]
-    system "cmake", "-S", ".", "-B", "build", "-DVCPKG_ROOT=#{vcpkg_root}", *std_cmake_args
+    resource("vcpkg").stage buildpath/"vcpkg"
+    
+    cd "vcpkg" do
+      system "./bootstrap-vcpkg.sh", "-disableMetrics"
+    end
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_TOOLCHAIN_FILE=#{buildpath}/vcpkg/scripts/buildsystems/vcpkg.cmake", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
