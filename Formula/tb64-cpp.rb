637ddba6746c8d83bdfc7407cef9b24b4d06185d
Frank Froggins <pearlover912@gmail.com>
2025-11-05 09:40:03 -0500

    Chore: Updated Formula

--- Formula/tb64-cpp.rb
+++ Formula/tb64-cpp.rb
@@ -1,20 +1,24 @@
 class Tb64Cpp < Formula
   desc "A niche tool to encode text in base64 3 times. (C++ version)"
   homepage "https://github.com/franktankbank/tb64-cpp"
-  url "https://github.com/franktankbank/tb64-cpp/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "fd951706e838925c3ca5c92443b5331dc81fc88fc135632de85df45608b7253a"
-  license "MIT"
+  url "https://github.com/franktankbank/tb64-cpp/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "5908b434874e9b183e7988455f51c88fa1284231f85b3b2f162c67713edb199f"
+  license "GPL-3.0-only"
 
   depends_on "cmake" => :build
+  depends_on "cpp-gsl" => :build
 
   resource "clip" do
-    url "https://github.com/dacap/clip/archive/refs/tags/v1.10.tar.gz"
-    sha256 "6b27976f0d1940697338f374a879964fff8fc02d0263faa76cfab99c9afff86f"
+    url "https://github.com/dacap/clip/archive/refs/tags/v1.12.tar.gz"
+    sha256 "54e96e04115c7ca1eeeecf432548db5cd3dddb08a91ededb118adc31b128e08c"
+  end
+
+  resource "turbo-base64" do
+    url "https://github.com/franktankbank/Turbo-Base64/archive/refs/heads/master.zip"
+    sha256 "49e66127b451c44a18bfd5d1c9f5ed79506c08c04db41338fc7926736e28396e"
   end
 
   def install
-    system "mkdir", "#{buildpath}/vcpkg"
-
     resource("clip").stage do
       mkdir "build" do
         system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_PREFIX=#{buildpath}/clip", "-DCLIP_ENABLE_IMAGE=OFF", "-DCLIP_EXAMPLES=OFF", "-DCLIP_TESTS=OFF", "-DCMAKE_BUILD_TYPE=Release"
@@ -22,14 +26,16 @@ class Tb64Cpp < Formula
         system "cmake", "--install", "."
       end
     end
-    
-    cd "vcpkg" do
-      system "git", "init"
-      system "git", "remote", "add", "upstream", "https://github.com/microsoft/vcpkg.git"
-      system "git", "pull", "upstream", "master"
-      system "./bootstrap-vcpkg.sh", "-disableMetrics"
+
+    resource("turbo-base64").stage do
+      mkdir "build" do
+        system "cmake", "..", *std_cmake_args, "-DCMAKE_INSTALL_PREFIX=#{buildpath}/turbo-base64", "-DCMAKE_BUILD_TYPE=Release"
+        system "cmake", "--build", "."
+        system "cmake", "--install", "."
+      end
     end
-    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_TOOLCHAIN_FILE=#{buildpath}/vcpkg/scripts/buildsystems/vcpkg.cmake", "-DHOMEBREW=ON", "-DCMAKE_BUILD_TYPE=Release", "-DCLIP_ROOT=#{buildpath}/clip", *std_cmake_args
+
+    system "cmake", "-S", ".", "-B", "build", "-DHOMEBREW=ON", "-DCMAKE_BUILD_TYPE=Release", "-DCLIP_ROOT=#{buildpath}/clip", "-DTURBO_BASE64_ROOT=#{buildpath}/turbo-base64", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
